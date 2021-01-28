6250fc5a7eb398433ea6548dc5cb287a65518b4a
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-28 22:07:23 +0000

    Update tools.lua

--- tools.lua
+++ tools.lua
@@ -1,30 +1,16 @@
-minetest.register_tool("froager2:green_energized_supremium_pickaxe", {
-    description = "Green Energized Supremium Pickaxe",
-    inventory_image = "green_energized_supremium_pickaxe.png",
-    tool_capabilities = {
-        max_drop_level=3,
-        groupcaps= {
-            cracky={times={[1]=1.00, [2]=0.375, [3]=0.25}, uses=170, maxlevel=1}
-        }
-    }
-})
-minetest.register_tool("froager2:blue_energized_supremium_pickaxe", {
-    description = "Blue Energized Supremium Pickaxe",
-    inventory_image = "blue_energized_supremium_pickaxe.png",
-    tool_capabilities = {
-        max_drop_level=3,
-        groupcaps= {
-            cracky={times={[1]=1.00, [2]=0.375, [3]=0.25}, uses=170, maxlevel=1}
-        }
-    }
-})
-minetest.register_tool("froager2:red_energized_supremium_pickaxe", {
-    description = "Red Energized Supremium Pickaxe",
-    inventory_image = "red_energized_supremium_pickaxe.png",
-    tool_capabilities = {
-        max_drop_level=3,
-        groupcaps= {
-            cracky={times={[1]=1.00, [2]=0.375, [3]=0.25}, uses=170, maxlevel=1}
-        }
-    }
-})
+local function register_energixed_supremium_pick(color, description)
+  minetest.register_tool("froager2:" .. color .. "_energized_supremium_pickaxe", {
+      description = description,
+      inventory_image = color .. "_energized_supremium_pickaxe.png",
+      tool_capabilities = {
+          max_drop_level=3,
+          groupcaps= {
+              cracky={times={[1]=1.00, [2]=0.375, [3]=0.25}, uses=170, maxlevel=1}
+          }
+      }
+  })
+end
+
+register_energixed_supremium_pick("red", "Red Energized Supremium Pickaxe")
+register_energixed_supremium_pick("green", "Green Energized Supremium Pickaxe")
+register_energixed_supremium_pick("blue", "Blue Energized Supremium Pickaxe")
