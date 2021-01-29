313dc24d738d096d9fe320c69225f297180d7fca
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-30 00:22:06 +0100

    push untested biome code

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ minetest.register_on_joinplayer(function(ObjectRef)
     minetest.chat_send_player(ObjectRef:get_player_name(), "Report bugs and request features in my discord server: https://discord.gg/MmdyHUHUuZ")
 end)
 
-local modules = {"items", "crafts", "ovencrafts", "weapons", "tools", "blocks", "ores"}
+local modules = {"items", "crafts", "ovencrafts", "weapons", "tools", "blocks", "ores", "biomes"}
 
 for _, module in pairs(modules) do
     dofile(modpath .. "/" .. module .. ".lua")
