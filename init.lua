dfb24c4a9fa46f7f12a4845ef82075f3f6205705
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-29 08:53:28 +0000

    optimise init.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,12 @@
-local path = minetest.get_modpath("froager2")
+local modpath = minetest.get_modpath("froager2")
 
 minetest.register_on_joinplayer(function(ObjectRef)
     minetest.chat_send_player(ObjectRef:get_player_name(), "Froager 2 - by Zander_200")
     minetest.chat_send_player(ObjectRef:get_player_name(), "Report bugs and request features in my discord server: https://discord.gg/MmdyHUHUuZ")
 end)
 
---items
-dofile(path.."/items.lua")
---crafts
-dofile(path.."/crafts.lua")
---oven
-dofile(path.."/ovencrafts.lua")
---weapons
-dofile(path.."/weapons.lua")
---tools
-dofile(path.."/tools.lua")
---blocks
-dofile(path.."/blocks.lua")
---ores
-dofile(path.."/ores.lua")
+local modules = {"items", "crafts", "ovencrafts", "weapons", "tools", "blocks", "ores"}
+
+for _, module in pairs(modules) do
+    dofile(modpath .. "/" .. module .. ".lua")
+end
