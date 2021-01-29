0147eb9d3c13b5ac4e1d257025f11a34900358ae
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-30 00:16:08 +0100

    totally not complete biome lol

---
+++ biomes.lua
@@ -0,0 +1,8 @@
+minetest.register_biome({
+    name = "Insolated Biome",
+    node_top = "froager2:dirt_energized",
+    depth_top = 1,
+    node_filler = "froager:dirt",
+    depth_filler = 5,
+
+})
