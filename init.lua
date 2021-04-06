414b7902ac88159d9417e7c3523a212f8892b1af
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-04-06 10:53:33 +0200

    Add test craft to essencecrafts

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ minetest.register_on_joinplayer(function(ObjectRef)
     minetest.chat_send_player(ObjectRef:get_player_name(), "Report bugs and request features in my discord server: https://discord.gg/MmdyHUHUuZ")
 end)
 
-local modules = {"items", "crafts", "ovencrafts", "weapons", "tools", "blocks", "ores", "biomes", "forge", "hammer"}
+local modules = {"items", "crafts", "ovencrafts", "weapons", "tools", "blocks", "ores", "biomes", "forge", "hammer", "essence_crafts"}
 
 for _, module in pairs(modules) do
     dofile(modpath .. "/" .. module .. ".lua")
