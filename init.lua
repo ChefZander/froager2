8384813605d67988741105c52214ad3fdb21cad2
Chef Zander <tobias.aha2008@gmail.com>
2022-08-27 19:09:09 +0200

    vcs commit test

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local modpath = minetest.get_modpath("froager2")
 
 minetest.register_on_joinplayer(function(ObjectRef)
-    minetest.chat_send_player(ObjectRef:get_player_name(), "Froager 2 - by Zander_200")
+    minetest.chat_send_player(ObjectRef:get_player_name(), "Froager 2 - by Zander")
     minetest.chat_send_player(ObjectRef:get_player_name(), "Report bugs and request features in my discord server: https://discord.gg/MmdyHUHUuZ")
 end)
 
