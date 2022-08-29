c17b347fbcbd1aff14dec3f7e4398a08c8dfb6fd
Chef Zander <tobias.aha2008@gmail.com>
2022-08-29 22:24:25 +0200

    yes it works now youre stranded in space lmaoooo

--- voidworld.lua
+++ voidworld.lua
@@ -5,15 +5,20 @@ minetest.register_node("froager2:spacestation_controller", {
     on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
         if clicker:is_player() then
             local ppos = clicker:get_pos()
+            minetest.load_area({x=ppos.x, y=30000, z=ppos.z})
             local node = minetest.get_node({x=ppos.x, y=30000, z=ppos.z})
             if node.name == "air" then
                 minetest.set_node({x=ppos.x, y=30000, z=ppos.z}, { name = "default:cobble" })
             end
-
+            clicker:move_to({x=ppos.x, y=30001, z=ppos.z}, false)
         end
     end,
 })
 
 minetest.register_globalstep(function(dtime)
-    
+    for _, player in ipairs(minetest.get_connected_players()) do
+        if player:get_pos().y > 29000 then
+            minetest.chat_send_player(player:get_player_name(), "U IN SPACE WTFFF")
+        end
+    end
 end)
