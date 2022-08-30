58a856307ff4cf5b1ee0240fe8f2d904bfd47232
Chef Zander <tobias.aha2008@gmail.com>
2022-08-30 18:26:07 +0200

    it works (also i added luacheck config)

--- voidworld.lua
+++ voidworld.lua
@@ -11,14 +11,38 @@ minetest.register_node("froager2:spacestation_controller_fueled", {
 	groups = {cracky = 3},
     on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
         if clicker:is_player() then
-            local ppos = clicker:get_pos()
-            minetest.load_area({x=ppos.x, y=30000, z=ppos.z})
-            local node = minetest.get_node({x=ppos.x, y=30000, z=ppos.z})
-            if node.name == "air" then
-                minetest.set_node({x=ppos.x, y=30000, z=ppos.z}, { name = "default:cobble" })
+            if clicker:get_pos().y > 29000 then
+                -- is in space
+                minetest.chat_send_player(clicker:get_player_name(), "Deorbiting...")
+                local ppos = clicker:get_pos()
+                local spawnFound = false
+                local spawnRes = nil
+                local yIncrement = 0
+                while not spawnFound do
+                    local searchPos = {x=ppos.x, y=yIncrement, z=ppos.z}
+                    minetest.load_area(searchPos)
+                    if minetest.get_node(searchPos).name == "air" then
+                        spawnRes = searchPos
+                        spawnFound = true
+                    else
+                        yIncrement = yIncrement + 1
+                    end
+                end
+
+                clicker:move_to(spawnRes, false)
+                minetest.set_node(pos, {name = "froager2:spacestation_controller"})
+            else
+                -- not in space
+                minetest.chat_send_player(clicker:get_player_name(), "Launching...")
+                local ppos = clicker:get_pos()
+                minetest.load_area({x=ppos.x, y=30000, z=ppos.z})
+                local node = minetest.get_node({x=ppos.x, y=30000, z=ppos.z})
+                if node.name == "air" then
+                    minetest.set_node({x=ppos.x, y=30000, z=ppos.z}, { name = "default:cobble" })
+                end
+                clicker:move_to({x=ppos.x, y=30001, z=ppos.z}, false)
+                minetest.set_node(pos, {name = "froager2:spacestation_controller"})
             end
-            clicker:move_to({x=ppos.x, y=30001, z=ppos.z}, false)
-            minetest.set_node(pos, {name = "froager2:spacestation_controller"})
         end
     end,
 })
